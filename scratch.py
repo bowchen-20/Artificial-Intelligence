035845433288908deaac7e61e7be015dc1c703f2
bowchen-20 <63386938+bowchen-20@users.noreply.github.com>
2021-12-06 17:23:40 -0500

    Delete scratch.py

--- scratch.py
+++
@@ -1,4 +0,0 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-data = "ENIAC's construction was financed by the United States Army, Ordnance Corps, led by Major General Gladeon M. Barnes."
-print(word_tokenize(data))
